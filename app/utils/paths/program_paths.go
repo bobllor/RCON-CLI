d119e79453ef15429b4c361f19bab9caf836165d
teboc <bobllor@yahoo.com>
2026-07-12 17:33:23 -0500

    add log path var

--- app/utils/paths/program_paths.go
+++ app/utils/paths/program_paths.go
@@ -7,3 +7,7 @@ const ConfigPathRel = ".config/mcrcon"
 // RuntimePathRel is the relative path to the runtime files
 // for the program.
 const RuntimePathRel = ".local/share/gorcon"
+
+// LogPathRel is the relative path to the log files
+// for the program.
+const LogPathRel = ".local/state/gorcon-logs"
