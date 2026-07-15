12ed760c907d598ddc8788eb40ab7c33c24485ff
teboc <bobllor@yahoo.com>
2026-07-15 01:16:52 -0500

    update ErrNotRconServer message

--- rcon/errors.go
+++ rcon/errors.go
@@ -3,4 +3,4 @@ package rcon
 import "errors"
 
 var ErrAuthFail = errors.New("failed to authenticate, wrong password")
-var ErrNotRconServer = errors.New("connected server is not an RCON server")
+var ErrNotRconServer = errors.New("server address is not an RCON server")
