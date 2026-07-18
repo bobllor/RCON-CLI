77ee58b145fd368dcdc927a20d080f08c77ffc04
bobllor <bobllor@yahoo.com>
2026-07-18 16:04:04 -0500

    add basic info for ps1 script

---
+++ install.ps1
@@ -0,0 +1,12 @@
+<#
+    .DESCRIPTION
+    Install gorcon onto a Windows device.
+
+    .AUTHOR
+    bobllor
+#>
+
+$file_name="gorcon.windows.amd64.zip"
+
+$baseUrl = "https://github.com/bobllor/rcon-cli/releases/latest"
+$url="$base_url/download/$file_name"
