391aecc288ed90f7b73871b5f5c9adf01b2b2b09
bobllor <bobllor@yahoo.com>
2026-07-19 23:29:13 -0500

    finish install.ps1

--- install.ps1
+++ install.ps1
@@ -6,33 +6,58 @@
     bobllor
 #>
 
-$file_name="gorcon.windows.amd64.zip"
+$fileName="gorcon.windows.amd64.zip"
 
 $baseUrl = "https://github.com/bobllor/rcon-cli/releases/latest"
-$url="$base_url/download/$file_name"
+$url="$baseUrl/download/$fileName"
 
-
-# speeds up webrequest download, just windows things
-$ProgressPreference = 'SilentlyContinue'
+echo "Starting gorcon installation..."
+echo ""
 
 $tempPath = "$env:temp\gorcontmp"
 
 if(!(Test-Path "$tempPath")){
-    mkdir "$tempPath"
+    mkdir "$tempPath" | out-null
 }
 
 # will just let this fail if an error occurs, better for
 # user logging
-Invoke-WebRequest -uri "$url" -usebasicparsing -o "$tempPath\$file_name"
+echo "Downloading $fileName..."
+try{
+    Invoke-WebRequest -uri "$url" -usebasicparsing -o "$tempPath\$fileName"
+}catch{
+    Write-Error $_
+    exit 1
+}
 
-$appPath = "$env:localappdata\gorcon"
+$appPath = "$env:localappdata\Programs\gorcon"
 
 if(!(Test-Path "$appPath")){
-    mkdir "$appPath"
+    mkdir "$appPath" | out-null 
 }
 
-Expand-Archive "$tempPath\$file_name" "$appPath"
+echo "Extracting $fileName..."
+Expand-Archive "$tempPath\$fileName" "$appPath" -Force
+echo "Extracted files to $appPath"
+
+$userEnvPaths = [System.Environment]::GetEnvironmentVariable("PATH", "User")
+
+if(!($userEnvPaths.split(";") -contains "$appPath")){
+    echo "Configuring PATH..."
+    $userEnvPaths += "$appPath;"
+
+    echo "$userEnvPaths"
+    [System.Environment]::SetEnvironmentVariable("PATH", "$userEnvPaths", "User")
+
+    echo "PATH configured ($appPath)"
+}else{
+    echo "PATH already configured, skipping"
+}
 
 if(Test-Path "$tempPath"){
-    rm -Force "$tempPath"
+    rm -Force -Recurse "$tempPath"
 }
+
+echo ""
+echo "Successfully installed!"
+echo 'Open a new terminal to load the updated PATH and run "gorcon" to get started'
