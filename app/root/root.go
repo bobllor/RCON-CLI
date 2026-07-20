f123b9bbf6220d58b48b71c531c5c1733e8c5dfb
teboc <bobllor@yahoo.com>
2026-07-19 23:36:44 -0500

    removed running code from gorcon, add version to built-in cobra.Command

--- app/root/root.go
+++ app/root/root.go
@@ -1,8 +1,6 @@
 package root
 
 import (
-	"fmt"
-
 	"github.com/bobllor/rcon-cli/app/utils/paths"
 	"github.com/spf13/cobra"
 )
@@ -19,13 +17,7 @@ var ProgramVersion = "v1.0.0"
 // if one exists.
 type RootCommand struct {
 	Cmd  *cobra.Command
-	data RootData
 	Path paths.AppPath
-}
-
-type RootData struct {
-	// Version is used as a flag to show the version of the tool.
-	Version bool
 }
 
 // NewRootCommand creates a new RootCommand and its initialization flags.
@@ -35,27 +27,11 @@ func NewRootCommand(appPaths paths.AppPath) *RootCommand {
 			Use:   "gorcon",
 			Short: "Execute a command with RCON",
 		},
-		data: RootData{},
 		Path: appPaths,
 	}
 
-	cmd.Cmd.Run = cmd.Run
 	cmd.Cmd.CompletionOptions.DisableDefaultCmd = true
-
-	cmd.RootInitFlags()
+	cmd.Cmd.Version = ProgramVersion
 
 	return cmd
-}
-
-// Run is the main entry point for the root CMD. This will run
-// the execution of the command to the server.
-func (r *RootCommand) Run(cmd *cobra.Command, args []string) {
-	if r.data.Version {
-		fmt.Println(ProgramVersion)
-		return
-	}
-}
-
-func (r *RootCommand) RootInitFlags() {
-	r.Cmd.Flags().BoolVarP(&r.data.Version, "version", "v", false, "Displays the version")
 }
