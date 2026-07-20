a95f0a2fe49cc6b9f8c7c89362a4854ad74e82b2
bobllor <bobllor@yahoo.com>
2026-07-19 23:29:39 -0500

    update output messages

--- install.sh
+++ install.sh
@@ -6,8 +6,6 @@ os=$(uname -s)
 
 file_name=""
 
-echo -e "Starting gorcon installation...\n"
-
 case "$os" in
     Linux)
         file_name="gorcon.linux.amd64.tar.gz"
@@ -20,6 +18,8 @@ case "$os" in
         exit 1
         ;;
 esac
+
+echo -e "Starting gorcon installation...\n"
 
 base_url="https://github.com/bobllor/rcon-cli/releases/latest"
 url="$base_url/download/$file_name"
@@ -44,22 +44,23 @@ curl -L "$url" -o "$temp_folder/$file_name"
 
 echo ""
 
-echo "Extracting files..."
+echo "Extracting $file_name..."
 tar -xzf "$file_name" -C "$HOME/.local/bin"
+echo "Extracted files to $HOME/.local/bin"
 
 bash_file="$HOME/.bashrc"
 
 if [[ -z $(cat "$bash_file" | grep 'export PATH="$PATH:$HOME/.local/bin"') ]]; then
-    echo "PATH not found"
     echo "Configuring PATH..."
 
     echo "" >> "$bash_file"
     echo 'export PATH="$PATH:$HOME/.local/bin"' >> "$bash_file"
 
     echo "Configured PATH ($HOME/.local/bin)"
+else
+    echo "PATH already configured, skipping"
 fi
 
-echo "Cleaning up..."
 rm -rf "$temp_folder"
 
 source "$bash_file"
